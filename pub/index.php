7e3af2eb4260fb5f45ade186d225e7613c8be28e
bhhthinh <thinhcuaban@gmail.com>
2023-03-23 11:39:58 +0700

    fix CORS

--- pub/index.php
+++ pub/index.php
@@ -7,18 +7,6 @@
  */
 use Magento\Framework\App\Bootstrap;
 
-$method = $_SERVER['REQUEST_METHOD'];
-if ($method == "OPTIONS") {
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-    header("HTTP/1.1 200 OK");
-    die();
-}
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
 try {
     require __DIR__ . '/../app/bootstrap.php';
 } catch (\Exception $e) {
